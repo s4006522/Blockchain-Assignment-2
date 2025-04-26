e96e97d6786c5b31a416eee97a21d0bd886eed5a
marceneo <s4007326@student.rmit.edu.au>
2025-04-26 17:59:12 +1000

    Flask Set up

--- ProjectSrcCode.py
+++ ProjectSrcCode.py
@@ -1,9 +1,18 @@
 # Task 1
 # Part 1 Creating PK and SK
-
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import hashlib
 import json
 import os
+
+app = Flask(__name__) # Set up Flask app
+
+@app.route('/') # Defining the route for the index page
+def index():
+    return render_template('index.html')
+
+app.run(debug=True)
+
 
 # # Opening and using json to retrieve the data
 # with open('list_of_keys.json', 'r') as file:
@@ -46,3 +55,63 @@ import os
 #     json.dump(a_pk_sk, file, indent=3)
 
 # signing each message 
+
+def inventory_a_record_creation():
+    # Creating a record for inventory A
+    message = {
+        "inventory_a": {
+            "item_id": "004",
+            "item_qty": 12,
+            "iten_price": 18,
+            "location": "A"
+        }
+    }
+
+    with open('inventory_a.json', 'w') as file:
+        json.dump(message, file, indent=3)
+inventory_a_record_creation()
+
+def inventory_b_record_creation():
+    # Creating a record for inventory A
+    message = {
+        "inventory_b": {
+            "item_id": "004",
+            "item_qty": 12,
+            "iten_price": 18,
+            "location": "B"
+        }
+    }
+
+    with open('inventory_b.json', 'w') as file:
+        json.dump(message, file, indent=3)
+inventory_b_record_creation()
+
+def inventory_c_record_creation():
+    # Creating a record for inventory A
+    message = {
+        "inventory_c": {
+            "item_id": "004",
+            "item_qty": 12,
+            "iten_price": 18,
+            "location": "C"
+        }
+    }
+
+    with open('inventory_c.json', 'w') as file:
+        json.dump(message, file, indent=3)
+inventory_c_record_creation()
+
+def inventory_d_record_creation():
+    # Creating a record for inventory A
+    message = {
+        "inventory_d": {
+            "item_id": "004",
+            "item_qty": 12,
+            "iten_price": 18,
+            "location": "D"
+        }
+    }
+
+    with open('inventory_d.json', 'w') as file:
+        json.dump(message, file, indent=3)
+inventory_d_record_creation()
