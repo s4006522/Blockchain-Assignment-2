c0e86b0f0be83e4bc30cf8335b7b4e18c06810d9
Anusha Kant <s4006522@student.rmit.edu.au>
2025-05-16 06:00:35 +1000

    Query Search/Submission Implemented

--- task_3.py
+++ task_3.py
@@ -14,16 +14,27 @@ def index():
 @app.route('/task3', methods=['POST']) # Defining the route for the verify page
 def task3():
     search_query = request.form["search_query"]
+    result = {}
+
     inventory = ['a','b','c','d']
     for i in inventory:
-        with open(f'inventory_{i}.json', 'r') as f:
-            key = json.load(f)
-            key = key
+        file = f"inventory_{i}.json"
+        key = f"inventory_{i}"
     
-    return render_template(
-        'task3.html',
-        key = key
-        )
+        try:
+            with open(file, 'r') as f:
+                data = json.load(f)
+                inventory_data = data.get(key, {})
+
+                for idx, record in inventory_data.items():
+                    record_id = record[:3]  # Get only the first 3 characters
+                    if record_id == search_query:
+                        result[i.upper()] = record
+        except FileNotFoundError:
+            flash(f"File {file} not found.", "error")
+            continue
+
+    return render_template("task3.html", search_query=search_query, results=result)
     # Get the message and signature from the form
 # THIS IS  ATEST
 
